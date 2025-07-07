b9670238f37e59c49f63c1d5d4fb6220a99e3c6c
aldian <aldian@gai.co.id>
2025-07-07 17:49:05 +0700

    add function get_current_user_optional, for validate of API when this route can handle with or without token.

--- baseapp/utils/jwt.py
+++ baseapp/utils/jwt.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 import logging
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 from fastapi import Depends, HTTPException, Request
 from fastapi.security import OAuth2PasswordBearer
 from jose import ExpiredSignatureError, JWTError, jwt
@@ -34,7 +34,7 @@ def create_refresh_token(data: dict, expire_in: int = 7) -> tuple:
 def decode_jwt_token(token: str) -> Dict[str, Any]:
     return jwt.decode(token, jwt_secret_key, algorithms=[jwt_algorithm])
 
-def get_current_user(ctx: Request, token: str = Depends(OAuth2PasswordBearer(tokenUrl="v1/auth/token"))) -> CurrentUser:
+def _get_current_user(ctx: Request, token: str = Depends(OAuth2PasswordBearer(tokenUrl="v1/auth/token"))) -> CurrentUser:
     def credentials_exception(message: str):
         return HTTPException(
             status_code=401,
@@ -83,3 +83,11 @@ def get_current_user(ctx: Request, token: str = Depends(OAuth2PasswordBearer(tok
         ip_address=ctx.client.host,
         user_agent=ctx.headers.get("user-agent")
     )
+
+def get_current_user(ctx: Request, token: str = Depends(OAuth2PasswordBearer(tokenUrl="v1/auth/token"))) -> CurrentUser:
+    return _get_current_user(ctx, token)
+
+def get_current_user_optional(ctx: Request, token: Optional[str] = Depends(OAuth2PasswordBearer(tokenUrl="v1/auth/token", auto_error=False))) -> Optional[CurrentUser]:
+    if token is None:
+        return None
+    return _get_current_user(ctx, token)
